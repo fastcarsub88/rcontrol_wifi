983b2bf7396681b424ecbe0890a1396d73858334
jordan <jordankhn88@gmail.com>
2023-04-05 08:46:48 -0500

    frontend updates

--- html/index.js
+++ html/index.js
@@ -1,45 +1,10 @@
 var params,serverTime,d_stat,weather;
 var errors = '';
-var modal = document.getElementById('mdl_elem');
 var parModel = document.getElementById('par_mdl_elem');
-var stModal = document.getElementById('st_mdl_elem');
-var setAutoBtn = document.getElementById('auto_btn');
-var modalSlider = document.getElementById('amount_mv');
-var modalBtn = document.getElementById('modal_btn');
 var loader = document.getElementById('loader_elem');
-var mdlFuncVl = document.getElementById('mdl_func_vl');
-var modalSliderDiv = document.getElementById('mdl_sldr_div');
+var auto_checkboxes = document.getElementById('auto_checkboxes')
 var parForm = document.forms.par_form;
-var opStForm = document.forms.op_st_form;
 
-modal.open = async function(func,dnum){
-  if (func == 'set_pressure') {
-    var title = "Set Pressure";
-    modalBtn.innerText = 'Set';
-    var data = await get_params();
-    pres = data.max_pres*10;
-    modalSlider.value = pres;
-    mdlFuncVl.innerText = pres+"%";
-    modalSlider.max = '100'
-    modalSlider.OnMove = () => {
-      mdlFuncVl.innerText = modalSlider.value+'%'
-    }
-  }
-  if (func == 'set_auto') {
-    var title = "Set Auto"
-    modalSliderDiv.classList.add('no-display');
-    modalBtn.innerText = (setAutoBtn.dataset.dnum == '0' ? "ON" : "OFF");
-    modal.auto = (setAutoBtn.dataset.dnum == '0' ? "true" : "false");
-  }
-  document.getElementById('model_title').innerText = title;
-  this.func = func;
-  this.classList.remove('no-display')
-};
-modal.close = function(){
-  modalSliderDiv.classList.remove('no-display');
-  modal.func = '';
-  this.classList.add('no-display');
-};
 parModel.open = async function () {
   var data = await get_params();
   parForm.par_chbx_op_tm.value = data.open_method;
@@ -58,18 +23,18 @@ parModel.open = async function () {
   }
   parForm.par_min_temp.value = data.min_temp;
   parForm.par_sm_door_temp.value = data.sm_door_temp;
-  parForm.par_max_pres.value = data.max_pres*10;
-  parForm.par_min_pres.value = data.min_pres*10;
-  parForm.par_fst_close_tm.value = data.fst_close_tm;
-  parForm.par_rain_pres.value = data.rain_pres*10;
+  var div = document.createElement('div')
+  for (let [key, value] of Object.entries(d_stat)){
+    var num = Number(key.slice(-1))+1
+    var name = "Barn"+num
+    var checked = (params.auto[num-1] == '1'? true : false)
+    div.append(createCheckbox(name,checked))
+  }
+  auto_checkboxes.innerHTML = ''
+  auto_checkboxes.append(div)
   this.classList.remove('no-display');
 }
 parModel.close = function () {this.classList.add('no-display')}
-stModal.open = function () {
-  opStForm.open_state.value = params.open_state;
-  this.classList.remove('no-display')
-}
-stModal.close = function () {this.classList.add('no-display')}
 loader.show = function () {
   loader.timeout = setTimeout(() => {loader.classList.remove('no-display')},1000)
 }
@@ -77,20 +42,17 @@ loader.hide = function () {
   clearTimeout(loader.timeout)
   this.classList.add('no-display')
 }
-setAutoBtn.onclick = () => modal.open('set_auto');
-document.getElementById('auto_btn').onclick = () => {modal.open('set_auto')};
-document.getElementById('pres_btn').onclick = () => {modal.open('set_pressure')};
-document.getElementById('op_st_btn').onclick = () => {stModal.open()};
 document.getElementById('get_params_btn').onclick = () => parModel.open();
 document.getElementById('par_mdl_btn').onclick = () => {
   var e = parForm.elements;
+  var auto = ''
+  parForm.querySelectorAll('.auto_checkboxes').forEach((item, i) => {
+    auto += (item.checked ? '1' : '0')
+  });
   var obj = {
     min_temp : parseInt(e.par_min_temp.value),
     sm_door_temp: parseInt(e.par_sm_door_temp.value),
-    max_pres: parseInt(e.par_max_pres.value),
-    min_pres: parseInt(e.par_min_pres.value),
-    rain_pres: parseInt(e.par_rain_pres.value),
-    fst_close_tm: parseInt(e.par_fst_close_tm.value)
+    auto: auto
   };
   if (e.par_chbx_op_tm.value == 'time'){
     if (e.par_open_tm.value.length !== 5) {
@@ -120,13 +82,7 @@ document.getElementById('par_mdl_btn').onclick = () => {
   }
   put_params(obj);
 }
-document.getElementById('st_modal_btn').onclick = () => {
-  var d = {open_state : opStForm.open_state.value};
-  put_params(d).then(stModal.close());
-}
 document.getElementById('clx1').onclick = () => parModel.close();
-document.getElementById('clx2').onclick = () => modal.close();
-document.getElementById('clx3').onclick = () => stModal.close();
 document.addEventListener('visibilitychange',() => {
   if (document.visibilityState == 'hidden') {
     poll.pause();
@@ -135,29 +91,9 @@ document.addEventListener('visibilitychange',() => {
     poll.start();
   }
 })
-modalBtn.onclick = async function () {
-  var func = ()=>{};
-  var fd = new FormData();
-  if (modal.func == 'set_pressure') {
-    fd.append('percent',modalSlider.value);
-    fd.append('method','set_pressure')
-  }else if (modal.func == 'set_auto') {
-    var auto = modal.auto;
-    fd.append('method','set_auto');
-    fd.append('auto',auto);
-  }
-  loader.show();
-  await send_data(fd).then(() => {
-    modal.close();
-    loader.hide();
-  });
-  setTimeout(() => get_conditions(),500);
-}
 window.onclick = (e) => {
-  if (e.target == modal){modal.close()}
   if (e.target == parModel) {parModel.close()}
 }
-modalSlider.oninput = function () {this.OnMove()}
 var poll = {
   paused: false,
   start: async function () {
@@ -229,17 +165,15 @@ function update_elements() {
   document.getElementById('wind_sp_elem').innerText = weather.wind_speed;
   document.getElementById('wind_dir_elem').innerText = weather.wind_dir;
   document.getElementById('rain_elem').innerText = (weather.rain == 'true' ? "Yes": "No");
-  document.getElementById('time_elem').innerText = status.time;
+  document.getElementById('time_elem').innerText = serverTime;
   document.getElementById('error_message').innerText = errors;
-  if (params.auto == 1) {
-    setAutoBtn.classList.add('btn_active');
-    setAutoBtn.dataset.dnum = '1';
-  }else {
-    setAutoBtn.classList.remove('btn_active')
-    setAutoBtn.dataset.dnum = '0';
-  }
   for (let [key, value] of Object.entries(d_stat)){
     if (!value) {continue}
+    if (params.auto[key.slice(-1)] == 1) {
+      document.getElementById(key+'fieldset').classList.add('in_auto')
+    }else {
+      document.getElementById(key+'fieldset').classList.remove('in_auto')
+    }
     value.forEach((item, i) => {
       var el = document.getElementById(key+i);
       if (item == 'on') {
@@ -253,6 +187,19 @@ function update_elements() {
   }
   var d = new Date();
   document.getElementById('last_time').innerText = d.toLocaleTimeString();
+}
+function createCheckbox(name, checked) {
+  var div = document.createElement('div')
+  var label = document.createElement('label')
+  var input = document.createElement('input')
+  input.type = 'checkbox'
+  input.id = name
+  input.checked = checked
+  input.classList.add('auto_checkboxes')
+  label.htmlFor = name
+  label.innerText = name.charAt(0).toUpperCase()+name.substring(1)
+  div.append(label,input)
+  return div
 }
 function createBtn(name,node,relay) {
   var btn = document.createElement('button')
@@ -271,7 +218,10 @@ async function init() {
   for (let [key, value] of Object.entries(d_stat)){
     var fieldset = document.createElement('fieldset')
     var legend = document.createElement('legend')
-    legend.innerText = "Barn"+(Number(key.replace('node',''))+1)
+    fieldset.classList.add('btn_fieldset')
+    fieldset.id = key+'fieldset'
+    legend.classList.add('btn_legend')
+    legend.innerText = "Barn"+(Number(key.slice(-1))+1)
     fieldset.append(legend)
     if (!value) {
       var p = document.createElement('p')
@@ -280,6 +230,10 @@ async function init() {
     }else {
       fieldset.append(createBtn('Main',key,0),createBtn('Small',key,1))
     }
+    var span = document.createElement('span')
+    span.classList.add('auto_span')
+    span.innerText = 'Auto'
+    fieldset.append(span)
     div.append(fieldset)
   }
   document.getElementById('doorbtndiv').append(div)
