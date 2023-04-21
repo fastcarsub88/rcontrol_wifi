2af6243bfbe412cb84002e75ef549bbd0a651d19
jordan <jordankhn88@gmail.com>
2023-04-21 08:02:27 -0500

    .

--- html/sw.js
+++ html/sw.js
@@ -1,6 +1,6 @@
 var farmname = location.pathname.replace('/sw.js','')
 farmname = farmname.replace('/','')
-var cacheName = farmname+'rctlv48';
+var cacheName = farmname+'rctlv49';
 var filesToCache = [
   "index.js?"+cacheName,
   "style.css?"+cacheName,
